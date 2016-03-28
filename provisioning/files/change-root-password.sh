5d9fa0b0f34ca7f0eb449412e2807b91f6dc2fa8
Shunsuke Andoh <shunsuke.andoh@gmail.com>
2016-03-28 23:14:43 +0900

    MySQL 共通設定

---
+++ provisioning/files/change-root-password.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+new_pass='MyNewPass4@'
+mysql --defaults-file=/root/.tmp.my.cnf -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$new_pass' "
+
+umask 0077
+cat > /root/.my.cnf <<EOF
+[client]
+user=root
+password=$new_pass
+EOF
